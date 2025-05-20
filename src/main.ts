b880d7f2c90333f2a29f8ddf0e1a2a4f2f1ea745
chief <chief@chiefs-MacBook-Air.local>
2025-05-20 22:59:35 +0200

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true, // Or your React app's origin, or true for all
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
